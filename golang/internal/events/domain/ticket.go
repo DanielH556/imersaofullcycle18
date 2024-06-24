130772170dcbfa0f11765fb675d59de5bcb94319
Daniel Hossaki <daniel.hossaki@outlook.com>
2024-06-24 17:23:00 -0300

    more changes to go repo and domain

--- golang/internal/events/domain/ticket.go
+++ golang/internal/events/domain/ticket.go
@@ -6,17 +6,25 @@ import (
 	"github.com/google/uuid"
 )
 
+// Errors
+var (
+	ErrInvalidTicketKind = errors.New("invalid ticket kind")
+)
+
+// TicketKind represents the kind of a ticket.
 type TicketKind string
 
 const (
-	TicketKindHalf TicketKind = "half"
-	TicketKindFull TicketKind = "full"
+	TicketKindHalf TicketKind = "half" // Half-price ticket
+	TicketKindFull TicketKind = "full" // Full-price ticket
 )
 
-var (
-	ErrTicketPriceZero = errors.New("ticket price must be greater than 0")
-)
+// IsValidTicketKind checks if a ticket kind is valid.
+func IsValidTicketKind(ticketKind TicketKind) bool {
+	return ticketKind == TicketKindHalf || ticketKind == TicketKindFull
+}
 
+// Ticket represents a ticket for an event.
 type Ticket struct {
 	ID         string
 	EventID    string
@@ -25,9 +33,10 @@ type Ticket struct {
 	Price      float64
 }
 
+// NewTicket creates a new ticket with the given parameters.
 func NewTicket(event *Event, spot *Spot, ticketKind TicketKind) (*Ticket, error) {
-	if !IsValidTicketType(ticketKind) {
-		return nil, errors.New("invalid ticket type")
+	if !IsValidTicketKind(ticketKind) {
+		return nil, ErrInvalidTicketKind
 	}
 
 	ticket := &Ticket{
@@ -37,7 +46,6 @@ func NewTicket(event *Event, spot *Spot, ticketKind TicketKind) (*Ticket, error)
 		TicketKind: ticketKind,
 		Price:      event.Price,
 	}
-
 	ticket.CalculatePrice()
 	if err := ticket.Validate(); err != nil {
 		return nil, err
@@ -45,19 +53,17 @@ func NewTicket(event *Event, spot *Spot, ticketKind TicketKind) (*Ticket, error)
 	return ticket, nil
 }
 
-func IsValidTicketType(ticketKind TicketKind) bool {
-	return ticketKind == TicketKindHalf || ticketKind == TicketKindFull
-}
-
+// CalculatePrice calculates the price based on the ticket kind.
 func (t *Ticket) CalculatePrice() {
 	if t.TicketKind == TicketKindHalf {
 		t.Price /= 2
 	}
 }
 
+// Validate checks if the ticket data is valid.
 func (t *Ticket) Validate() error {
 	if t.Price <= 0 {
-		return ErrTicketPriceZero
+		return errors.New("ticket price must be greater than zero")
 	}
 	return nil
 }
